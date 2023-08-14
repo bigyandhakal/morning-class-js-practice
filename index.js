f7c72d529bf195c2a0f1e4ac664dd3768b3d4541
Bigyan Dhakal <dhakalbigyan0@gmail.com>
2023-08-14 09:17:51 +0545

    javascript interactions and variables

--- index.js
+++ index.js
@@ -7,3 +7,27 @@ console.log('Hello');
 long comment
 
 */
+
+// variables
+// var - ES5
+// let, const - ES6
+
+// keyword - var, let, const
+
+var person = 'John';
+person = 'Rock';
+
+let person1 = 'Cena';
+person1 = 'Shaw';
+
+const person2 = 'Johnny'; // cannot be changed
+
+// JS interactions
+
+alert("Hello world!");
+// person1 = prompt("What is your name?");
+const isLegal = confirm('Are you 18 years and above?');
+
+console.log({person, person1, person2, isLegal})
+
+
